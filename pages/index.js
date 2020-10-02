f15a20c6234c1a0356f1ccfbd3b84c53eab0df5c
fehol <felix.hollmann@gmx.net>
2020-10-02 16:23:53 +0200

    Landing Page done way to go

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,17 @@ export default function Home() {
   return (
     <div>
       <header className="header">
+        <select className="MenuButton">
+          <option value="Videos">Videos</option>
+          <option value="Shopping Cart">Shopping Cart</option>
+          <option value="Checkout">Checkout</option>
+        </select>
         <button className="MenuButton">MENU</button>
       </header>
       <main>
         <div className="wrapper">
           <h1 data-text="Slashback">Slashback</h1>
           <h2 data-text="VideoStore">Video Store</h2>
-          <a className="Triangle"></a>
         </div>
         <div className="graph-container">
           <div class="graph"></div>
